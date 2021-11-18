dfba1989f69a268b9979a8b0882b5a3684c4e46a
Pixelherz23 <zocker34298@gmail.com>
2021-11-18 09:17:25 +0100

    REST init and Swagger-UI init

---
+++ Code/REST/swagger.js
@@ -0,0 +1,12 @@
+{
+    "openapi": "3.0.0",
+    "info": {
+      "description": "(interaktive) Dokumenation der Rest API",
+      "version": "1.0.0",
+      "title": "REST-Dinder",
+      "license": {
+        "name": "MIT",
+        "url": "https://opensource.org/licenses/MIT"
+      }
+    }
+}
